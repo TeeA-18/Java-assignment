c90442d9654348b858a0a9d7ea79bf170f0d2b77
Tran  Tuan Anh <teesass1812@gmail.com>
2020-11-06 21:32:05 +0700

    Update problem class

--- src/Problem.java
+++ src/Problem.java
@@ -31,6 +31,75 @@ public class Problem {
         this.category = category;
         this.author = author;
     }
+
+    public String getProblemID() {
+        return problemID;
+    }
+
+    public void setProblemID(String problemID) {
+        this.problemID = problemID;
+    }
+
+    public String getProblemName() {
+        return problemName;
+    }
+
+    public void setProblemName(String problemName) {
+        this.problemName = problemName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getShortDescrip() {
+        return shortDescrip;
+    }
+
+    public void setShortDescrip(String shortDescrip) {
+        this.shortDescrip = shortDescrip;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return   problemID + ";" + problemName + ";" + date + ";" + shortDescrip + ";" + link + ";" + mark + ";" + category + ";" + author;
+    }
     
     
     
